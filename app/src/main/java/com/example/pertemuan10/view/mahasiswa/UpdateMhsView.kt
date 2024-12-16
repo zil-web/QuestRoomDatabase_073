ac366318f966f0a495893cef916e77055d5cf182
zil-web <126854462+zil-web@users.noreply.github.com>
2024-12-17 05:52:31 +0700

    Menambahkan fitur Update Mahasiswa dengan Snackbar dan Validasi: - Menambahkan `UpdateMhsView` untuk memperbarui data mahasiswa. - Menggunakan `Snackbar` untuk menampilkan pesan sukses atau error. - Menambahkan `LaunchedEffect` untuk memantau perubahan snackbarMessage dari ViewModel. - Mengintegrasikan validasi input dan penanganan update data melalui ViewModel. - Menyediakan navigasi ke halaman lain setelah data berhasil diperbarui. - Menambahkan `InsertBodyMhs` sebagai komponen form input untuk memperbarui data mahasiswa.

---
+++ app/src/main/java/com/example/pertemuan10/view/mahasiswa/UpdateMhsView.kt
@@ -0,0 +1,90 @@
+package com.example.pertemuan10.view.mahasiswa
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.activity10.ui.costumwidget.TopAppBar
+import com.example.activity10.ui.view.mahasiswa.InsertBodyMhs
+import com.example.activity10.ui.viewmodel.PenyediaViewModel
+import com.example.activity10.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Composable
+fun UpdateMhsView (
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+) {
+    val uiState = viewModel.updateUIState // Ambil UI State dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() } // Snackbar state
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect (uiState.snackBarMessage) {
+        println("LaunchedEffect triggered")
+        uiState.snackBarMessage?.let { message ->
+            println("Snackbar message received: $message")
+            coroutineScope.launch {
+                println("Launching coroutine for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+            )
+        }
+    ){ padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Isi Body
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent) //Update state di ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate() // Navigasi di main thread
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
